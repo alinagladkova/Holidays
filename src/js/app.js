da747fc74719d12ffd72f679321c5bdfda717421
Alina Gladkova <tina.leaf89@gmail.com>
2022-08-21 01:12:37 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+
+import "./modules/script.js";
+import "./modules/burger.js";
+
+/*import * as flsScript from "./modules/script.js";
+flsScript.name();*/
+//@@include('script.js')
+
+
+
+
